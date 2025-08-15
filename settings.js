68bb86908c7c103b9b705ecdbf20c660e095b109
Kellytech11 <kellymutai640@gmail.com>
2025-08-16 01:13:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;eY9QnICD#ljmRVbZqaI4tjEygqpMVDo2h9En8cABYHXZtgQB8jdA"
+  SESSION_ID: "SUBZERO-MD;;;eY9QnICD#ljmRVbZqaI4tjEygqpMVDo2h9En8cABYHXZtgQB8jdA" //Put your Session Id here
   OWNER_NUMBER: "254707078681, // Put Owner Number Here 
   PREFIX: "/", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
